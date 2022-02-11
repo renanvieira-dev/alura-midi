6657177ac9d3b3e59ed7e3fc782e078d4a47c8ff
Renan Vieira Nascimento <reenaanfc@gmail.com>
2022-02-11 14:47:54 -0400

    Prjeto alura midi

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function pom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function clap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+
+function tim(){
+    document.querySelector('#som_tecla_tim').play();
+}
+
+function puff(){
+    document.querySelector('#som_tecla_puff').play();
+}
+
+function splash(){
+    document.querySelector('#som_tecla_splash').play();
+}
+
+function toim(){
+    document.querySelector('#som_tecla_toim').play();
+}
+
+function psh(){
+    document.querySelector('#som_tecla_psh').play();
+}
+
+function tic(){
+    document.querySelector('#som_tecla_tic').play();
+}
+
+function tom(){
+    document.querySelector('#som_tecla_tom').play();
+}
